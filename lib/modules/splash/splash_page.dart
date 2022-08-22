c7b5c4f34d5d462c1ecb7ff1dd6944200276159e
César Hilário <cesar.hilario.dev@gmail.com>
2022-08-21 23:00:03 -0300

    feat: add splashscreen

---
+++ lib/modules/splash/splash_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Stack(children: [
+        Center(
+            child: Image.asset(
+          AppImages.union,
+          width: size.width * 0.8,
+        )),
+        Center(
+            child: Image.asset(
+          AppImages.logofull,
+          width: size.width * 0.6,
+        )),
+      ]),
+    );
+  }
+}
